a5f59c5364bcb064502529dbadceacea007cc1d5
Ricardo Hernandez Sanchez <132201@l0t470pf0v7k47.na.rccl.com>
2019-03-31 13:25:47 -0400

    Adding TradingView extension

---
+++ tradingview_extension/README.md
@@ -0,0 +1,2 @@
+# Barchart Extension
+Opens a barchart technical chart window for a given ticker
---
+++ tradingview_extension/tradingview.js
@@ -0,0 +1,57 @@
+//Copyright (c) 2013. Ricardo Hdz. All rights reserved.
+
+var Tradingview_Handler = {
+	/**
+	* Default TradingView endpoint.
+	*
+	* @type {string}
+	* @private
+	**/
+	_URL: 'https://www.tradingview.com/symbols/{ticker}/',
+
+    
+    /*
+    * Create a document node to paste content to.
+    * This is a hack as browsers do not let you interact
+    * directly with execCommand('paste') contents.
+    *
+    * @return {string}
+    * @private
+    */
+    _pasteHack: function () {
+        var textarea = document.querySelector('textarea') ||
+            document.body.appendChild(document.createElement('textarea'));
+        textarea.select();
+        document.execCommand('paste');
+        return textarea.value.split('\n');
+    },
+
+    /*
+    * Constructs the standard the lookup url
+    *
+    * @returns {string}
+    * @private
+    */
+    openUrls : function () {
+        var url = '';
+        var tickers = this._pasteHack();
+
+        if (tickers) {
+            for (var i = 0, t; (t = tickers[i]); i++) {
+                url = this._URL.replace(/{ticker}/g, t);
+                chrome.tabs.create({url: url});
+            }
+            
+        }
+    }
+};
+
+/**
+* Chrome extension listener for the onClick action event
+* on the extension icon.
+* 
+* @param {function}
+*/
+chrome.browserAction.onClicked.addListener(function(tab) {
+    Tradingview_Handler.openUrls();
+});
